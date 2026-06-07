d85c46e4bfe34104fa95117648c285378ca316e7
AnmolRajpoot <147535076+AnmolRajpoot@users.noreply.github.com>
2026-06-07 22:11:48 +0000

    varibles

---
+++ basics/4_variables.js
@@ -0,0 +1,11 @@
+const name = "anmol"
+const account_id = 12345
+let accountemail = "alpha@email.com"
+account_city = "nagaland"
+
+console.log(account_city)
+
+account_city = 12
+
+// console.log(account_id)
+console.log(account_city)
